ecd1cf0745637a44669b79a5495794a8f8c1ce21
Amy Yao <10084869812@qq.com>
2023-10-16 14:08:07 +0800

    first Comrace

---
+++ src/views/FollowView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an Follow page</h1>
+    <br><br>
+    <h1>Have Fun and Meet Pal.</h1>
+    
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
